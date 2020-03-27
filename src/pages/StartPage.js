199f0f7df01d802c51d8e5cc4774a5aa0a51c508
FioraS <60643571+FioraS@users.noreply.github.com>
2020-03-27 16:57:13 +0100

    Update StartPage.js

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSession } from '../services/User';
 import { createGame } from '../services/MasterGame';
+import Button from '../components/Button';
 
 
 const Start = () => {
   const { user } = useSession();
   return (
     <div>
-      <Link to="/create" onClick={() => createGame(user)}> Nouvelle partie
-      </Link>
+
+        <Link to="/create" onClick={() => createGame(user)}> 
+          <Button>Nouvelle partie</Button>
+        </Link>
+      
       <br />
-      <Link to="/join">
-        Rejoindre une partie
-      </Link>
+      
+        <Link to="/join"> 
+          <Button>Rejoindre une partie</Button>
+        </Link>
+      
     </div>
   );
 }
+
 
 export default Start;
