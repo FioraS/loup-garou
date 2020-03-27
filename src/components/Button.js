529424b9773368185dbc437d53d32f00599e44d7
FioraS <60643571+FioraS@users.noreply.github.com>
2020-03-27 16:58:32 +0100

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  text-decoration: none;
+  border:none;
+  border-radius: 3px;
+  cursor: pointer;
+  padding: 8px 16px;
+  margin: 15px;
+  fontSize: 20;
+  fontFamily: "Arial";
+  
+  color: #fff;
+  background: #F65151;
+  &:hover {
+    background: #C73A3A ;
+  }
+  `
 
-const Button = (props) => {
+const Button1 = (props) => {
   const { onClick, children } = props;
   return (<button onClick={onClick}> { children }</button>);
 }
